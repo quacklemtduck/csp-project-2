875f1d0b3315239b111d770c8053f65f44db7078
kaep <sebastianholmgaard@live.dk>
2024-04-29 11:34:59 +0200

    delete files

--- rust/src/forkjoin.rs
+++
@@ -1,8 +0,0 @@
-pub fn forkjoin(elements: &mut Vec<u32>) {
-    let num_workers = 1024;
-    let mut pool = Pool::new(num_workers);
-
-    //https://www.geeksforgeeks.org/concurrent-merge-sort-in-shared-memory/
-}
-
-
